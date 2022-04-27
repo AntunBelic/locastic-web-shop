eb865b33ba2c33e5f72b1e295e2e216494c7300c
AntunBelic <antunbelic93@gmail.com>
2022-04-27 14:53:35 +0200

    refactor of api call

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -17,9 +17,8 @@ export interface IWorkShopProps {
 }
 
 
+
 export const DataProvider = ({ children }: { children: ReactNode }) => {
-
-
 
     const [workshops, setWorkshops] = useState<IWorkShopProps[]>([]);
     const [error, setError] = useState<string>("");
@@ -28,7 +27,10 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
     const [value, setValue] = useState<string>("");
     const [cart, setCart] = useState([] as IWorkShopProps[]);
     const [openDrawer, setOpenDrawer] = useState<boolean>(false);
-    const [postError, setPostError] = useState<string>("")
+    const [postError, setPostError] = useState<string>("");
+
+    const urlAll = `http://localhost:3500/workshops?_page=${page}&_limit=9&_sort=date&_order=desc`;
+    const urlCategory = `http://localhost:3500/workshops?_page=${page}&_limit=9&_sort=date&_order=desc&category=${value}`;
 
     useEffect(() => {
         loadWorkshops(page, value);
@@ -45,35 +47,14 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
 
     const loadWorkshops = async (page: number, value: string) => {
         setLoading(true);
-        if (page === 1 && value) {
-            return await axios.get(`http://localhost:3500/workshops?_page=1&_limit=9&_sort=date&_order=desc&category=${value}`)
-                .then((response) => setWorkshops(response.data))
-                .catch((err) => setError(err.message))
-                .finally(() => {
-                    setLoading(false)
-                })
-        } else if (page === 1 && !value) {
-            return await axios.get(`http://localhost:3500/workshops?_page=1&_limit=9&_sort=date&_order=desc`)
-                .then((response) => setWorkshops(response.data))
-                .catch((err) => setError(err.message))
-                .finally(() => {
-                    setLoading(false)
-                })
-        } else if (page !== 1 && value) {
-            return await axios.get(`http://localhost:3500/workshops?_page=${page}&_limit=9&_sort=date&_order=desc&category=${value}`)
-                .then((response) => setWorkshops((prev) => [...prev, ...response.data]))
-                .catch((err) => setError(err.message))
-                .finally(() => {
-                    setLoading(false)
-                })
-        } else if (page !== 1 && !value) {
-            return await axios.get(`http://localhost:3500/workshops?_page=${page}&_limit=9&_sort=date&_order=desc`)
-                .then((response) => setWorkshops((prev) => [...prev, ...response.data]))
-                .catch((err) => setError(err.message))
-                .finally(() => {
-                    setLoading(false)
-                })
-        }
+
+        await axios.get(value ? urlCategory : urlAll)
+            .then((response) => { page === 1 ? setWorkshops(response.data) : setWorkshops((prev) => [...prev, ...response.data]) })
+            .catch((err) => setError(err.message))
+            .finally(() => {
+                setLoading(false)
+            })
+
     }
 
 
